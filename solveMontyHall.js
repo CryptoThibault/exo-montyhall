6125e20591abb62da6545b9a82461b1b27d49332
CryptoThibault <thibault.chalaoux@gmail.com>
2021-04-08 17:50:36 +0200

    funct

--- solveMontyHall.js
+++ solveMontyHall.js
@@ -1,17 +1,22 @@
 const { randomInt } = require('crypto')
-const Nbtry = 1000
-const defaultChoice = 2
-const defaultChange = true
 
-let count = 0
-for (let i = 0; i < Nbtry; i++) {
-  const gates = ['goat', 'goat', 'goat']
-  gates[randomInt(0, 3)] = 'car'
-  let choiceGate = gates[defaultChoice]
-  gates.splice(defaultChoice, 1)
-  const showIndex = gates[0] === 'goat' ? 0 : 1
-  gates.splice(showIndex, 1)
-  if (defaultChange) { choiceGate = gates[0] }
-  if (choiceGate === 'car') { count++ }
+function solveMontyHall(Try, userChange) {
+  const defaultChoice = 1
+  let count = 0
+  for (let i = 0; i < Try; i++) {
+    const gates = ['goat', 'goat', 'goat']
+    gates[randomInt(0, 3)] = 'car'
+    let choiceGate = gates[defaultChoice - 1]
+    gates.splice(defaultChoice, 1)
+    const showIndex = gates[0] === 'goat' ? 0 : 1
+    gates.splice(showIndex, 1)
+    if (userChange) { choiceGate = gates[0] }
+    if (choiceGate === 'car') { count++ }
+  }
+  console.log(count)
 }
-console.log(count)
+// output: Nb Wins
+solveMontyHall(100, false)
+solveMontyHall(100, true)
+solveMontyHall(1000, false)
+solveMontyHall(1000, true)
